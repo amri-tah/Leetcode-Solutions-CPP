fb3794a85044eeb5de9b1c4351bff0149ad7b60b
Amritha Nandini <111682039+amri-tah@users.noreply.github.com>
2024-12-08 14:54:50 +0530

    Time: 1 ms (70.9%), Space: 9.9 MB (29.02%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        return s==t;
+        if (s.size()!=t.size()) return false;
+        
+        int n = s.size();
+        unordered_map<char, int> hashmap;
+        for (int i=0; i<n; i++){
+            hashmap[s[i]]++;
+        }
+
+        for (int i=0; i<n; i++){
+            hashmap[t[i]]--;
+            if (hashmap[t[i]]==0) hashmap.erase(t[i]);
+        }
+
+        return hashmap.size()==0;
     }
 };
